e640fbcbbe6b3211b6d1d8776252ed953cc24444
DESKTOP-NITBD9M\aboma <abo.mahroq@gmail.com>
2022-05-12 08:39:42 +0300

    update

--- app/src/main/java/com/mrq/sendemail/MainActivity.java
+++ app/src/main/java/com/mrq/sendemail/MainActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.mrq.email.SendEmailAddress;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String username = "seven.arts.ku@gmail.com";
+    private static final String password = "fneyqxlrzjjdqbsi";
+    private static final String toEmailList = "abo.mahroq@gmail.com,abu.mahroq@hotmail.com";
+    private static final String emailSubject = "subject";
+    private static final String emailBody = "Hello";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SendEmailAddress address = new SendEmailAddress();
+        address.setListEmailReceiver(toEmailList);
+        address.setEmailSubject(emailSubject);
+        address.setEmailBody(emailBody);
+        address.sendEmail(this, username, password);
+
     }
 }
